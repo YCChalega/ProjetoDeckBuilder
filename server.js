03fc81f3c24d158bf4f6ab2f69972d3724d17d87
Yure Chalega <yure.chalega0020016279@edu.pe.senac.br>
2026-06-06 14:06:39 -0300

    Ambiente ok

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const deckRoutes = require('./routes/deckRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
